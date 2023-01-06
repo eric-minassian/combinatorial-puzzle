2b1371ea54917c3d259cd00e77ab1edf48cbbcde
mikes <mikes@ics.uci.edu>
2023-01-06 11:52:35 -0800

    Initial commit

---
+++ app/puzzleSolver.cpp
@@ -0,0 +1,8 @@
+#include "puzzleSolver.hpp"
+
+bool puzzleSolver(const std::string& s1, const std::string& s2, const std::string& s3, std::unordered_map<char, unsigned> & mapping)
+{
+	return true; // This is not the correct general solution. 
+}
+
+
